8a30d639a959c60b6d764face831adf1d6b139f8
jamik1997-sudo <jamik1997@gmail.com>
2026-08-02 13:31:32 +0500

    Delete frontend directory

--- frontend/components/Header.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-import { clearToken } from "@/lib/api";
-import { useRouter } from "next/navigation";
-export default function Header(){const router=useRouter();return <header className="topbar"><div className="topbar-inner"><div className="brand"><span className="dot"/>SLE Audit</div><button className="btn btn-light" onClick={()=>{clearToken();router.push('/login')}}>Выйти</button></div></header>}
